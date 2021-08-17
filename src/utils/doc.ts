d17b5be8e6ea6b054a659295fc72907f3870b9c1
wubin1989 <328454505@qq.com>
2021-08-17 12:07:53 +0800

    ...

--- src/utils/doc.ts
+++ src/utils/doc.ts
@@ -2,14 +2,18 @@ import { OpenAPIV3 } from 'openapi-types'
 import { RouteConfig } from 'vue-router'
 import Doc from '@/views/doc/index.vue'
 import Layout from '@/layout/index.vue'
-import _ from 'lodash'
+import _, { now } from 'lodash'
 
-export function endpoint2Key(endpoint: string) :string {
+export function endpoint2Key(endpoint: string): string {
   return endpoint.replace(/[{}]/ig, '_').replace(/[^a-zA-Z0-9_]/ig, '')
 }
 
-export function endpoint2ModuleName(endpoint: string) :string {
+export function endpoint2ModuleName(endpoint: string): string {
   return endpoint.split('/')[1]
+}
+
+function getFirstPart(endpoint:string) {
+  return `${endpoint2ModuleName(endpoint)}${(Date.now() + '').substring(8) + (+Math.random().toFixed(6)) * 1000000}`
 }
 
 export const paths2Route = (paths: OpenAPIV3.PathsObject): RouteConfig[] => {
@@ -98,8 +102,8 @@ export const paths2Route = (paths: OpenAPIV3.PathsObject): RouteConfig[] => {
   Object.keys(groupBy).forEach(key => {
     const routes = groupBy[key]
     const { endpoint } = routes[0].meta
-    const firstPart = endpoint2ModuleName(endpoint)
-    const moduleName = _.capitalize(firstPart)
+    const firstPart = getFirstPart(endpoint)
+    const moduleName = _.capitalize(key)
     const father: RouteConfig = {
       path: `/${firstPart}`,
       component: Layout,
@@ -130,11 +134,12 @@ export function paths2HomeTable(paths: OpenAPIV3.PathsObject): HomeTable[] {
   const table: HomeTable[] = []
   Object.keys(paths).sort().forEach(endpoint => {
     const pathItem = paths[endpoint]
-    const firstPart = endpoint2ModuleName(endpoint)
+    const _tag = endpoint2ModuleName(endpoint)
+    const firstPart = getFirstPart(endpoint)
     if (pathItem?.get) {
       const method = 'get'
       const summary = pathItem.get.summary || ''
-      let tag = firstPart
+      let tag = _tag
       if (pathItem.get.tags?.length) {
         tag = pathItem.get.tags[0]
       }
@@ -150,7 +155,7 @@ export function paths2HomeTable(paths: OpenAPIV3.PathsObject): HomeTable[] {
     if (pathItem?.post) {
       const method = 'post'
       const summary = pathItem.post.summary || ''
-      let tag = firstPart
+      let tag = _tag
       if (pathItem.post.tags?.length) {
         tag = pathItem.post.tags[0]
       }
@@ -166,7 +171,7 @@ export function paths2HomeTable(paths: OpenAPIV3.PathsObject): HomeTable[] {
     if (pathItem?.put) {
       const method = 'put'
       const summary = pathItem.put.summary || ''
-      let tag = firstPart
+      let tag = _tag
       if (pathItem.put.tags?.length) {
         tag = pathItem.put.tags[0]
       }
@@ -182,7 +187,7 @@ export function paths2HomeTable(paths: OpenAPIV3.PathsObject): HomeTable[] {
     if (pathItem?.delete) {
       const method = 'delete'
       const summary = pathItem.delete.summary || ''
-      let tag = firstPart
+      let tag = _tag
       if (pathItem.delete.tags?.length) {
         tag = pathItem.delete.tags[0]
       }
@@ -199,7 +204,7 @@ export function paths2HomeTable(paths: OpenAPIV3.PathsObject): HomeTable[] {
   return _.sortBy(table, [function(o) { return o.path }])
 }
 
-export function tagType(method: string) :string {
+export function tagType(method: string): string {
   switch (method) {
     case 'get': {
       return ''
